2529edc8b136fae3711030aadb0a490bc519a0d4
Олег Ермаков <oleg.ermakov@fogstream.ru>
2021-08-18 20:35:37 +0300

    Добавил vue router, tailwindcss.

---
+++ src/pages/sign-up.vue
@@ -0,0 +1,13 @@
+<template>
+здесь нужно разверстать форму регистрации, которая содержит поля <span class="text-blue-800">e-mail, password, password confirm</span>
+</template>
+
+<script>
+export default {
+  name: "sign-up"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import Home from './pages/home.vue'
+import SignUp from './pages/sign-up.vue'
+
+const routes = [
+    {path: '/', component: Home},
+    { path: '/sign-up', component: SignUp },
+]
+
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
