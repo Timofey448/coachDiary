8b64ffa57a4be4a124a17d9e8b1609d482d7f3b3
Timofey448 <73985784+Timofey448@users.noreply.github.com>
2022-04-12 21:37:08 +0300

    исправление target   ...api/auth/users на ...api

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://51.250.105.185:8000/api/auth/users",
+        target: "http://51.250.105.185:8000/api",
         changeOrigin: true,
         secure: false,
         // rewrite: (path) => path.replace(/^\/api/, ""),
