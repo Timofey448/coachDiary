ab90dd529771e6b0c7293c2eecd5d90384247f9b
Timofey448 <73985784+Timofey448@users.noreply.github.com>
2022-10-18 23:26:10 +0300

    заглавная буква логотипа

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen w-screen">
     <router-link to="/sign-up">
-      <img class="h-80 w-80" src="/assets/logo.png" alt="">
+      <img class="h-80 w-80" src="/assets/Logo.png" alt="">
     </router-link>    
   </div>
 </template>
