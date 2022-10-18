7486806674321d07a5ac7be08ea9100324bab345
Timofey448 <73985784+Timofey448@users.noreply.github.com>
2022-10-18 22:52:25 +0300

    исправление путей изображения

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen w-screen">
     <router-link to="/sign-up">
-      <img class="h-80 w-80" src="../../public/assets/logo.svg" alt="">
+      <img class="h-80 w-80" src="/assets/logo.svg" alt="">
     </router-link>    
   </div>
 </template>
